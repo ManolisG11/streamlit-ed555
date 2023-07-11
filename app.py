505be0f3dd6f1bd69a26bc992842f35d22d2461e
emgeorgat <137608142+ManolisG11@users.noreply.github.com>
2023-07-11 14:40:25 +0300

    The app's source code

---
+++ app.py
@@ -0,0 +1,53 @@
+#Libraries
+import streamlit as st
+import pyautogui
+
+#Configure the page
+st.set_page_config(page_title="Encryptor-Decryptor", page_icon=":fingerprint", layout="wide")
+
+#Functions
+def encryptor(phrase):
+    encrypted_phrase = ""
+    for letter in phrase:
+        n = ord(letter)
+        n2 = n + key
+        encrypted_c = chr(n2)
+        encrypted_phrase = encrypted_phrase + encrypted_c
+    return encrypted_phrase
+
+def decryptor(phrase):
+    decrypted_phrase = ""
+    for letter in phrase:
+        n = ord(letter)
+        n2 = n - key
+        decrypted_c = chr(n2)
+        decrypted_phrase = decrypted_phrase + decrypted_c
+    return decrypted_phrase
+
+#Variables
+key = 555
+
+#Main
+st.title("Phrase Encryptor/Decryptor :fingerprint")
+
+password = st.text_input('Password')
+if password == "key555":
+    eord = st.text_input('What do you want to do:(E/D)')
+    if eord == "E":
+        user_word = st.text_area('Type something to encrypt:')
+
+        encrypted_word = encryptor(user_word)
+        st.success(encrypted_word)
+    elif eord == "D":
+        user_word = st.text_area('Type an encrypted phrase/word to decrypt it:')
+        decrypted_word = decryptor(user_word)
+        st.success(decrypted_word)
+        if st.button("Reset"):
+            pyautogui.hotkey("ctrl","F5")
+
+    else:
+        st.warning('Please enter a valid action (E = Encrypt, D = Decrypt)')
+elif password == "":
+    st.warning('Please enter a password')
+else:
+    st.error('Wrong Password')
